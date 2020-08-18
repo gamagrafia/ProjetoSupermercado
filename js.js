b0d6cafe2366b1e77ef3b32e0a2f4c17d3e39709
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-08-18 19:15:15 -0300

    Adicionando produtos ao Carrinho e somando dentro dele produtos iguais

--- js.js
+++ js.js
@@ -7,15 +7,34 @@ class Carrinho{
         this.itensDoCarrinho.push(produto)
         return produto
     }
+    // removerProdutoCarrinho(produtoid){
+        
+    // }
+
     get listaProdutosdoCarrinho(){
         return this.itensDoCarrinho
     } 
-    get mostrarValorTotal(){
-        return this.itensDoCarrinho.map(item => item.total) 
-        
+    get calculaValorTotal(){
+        return this.itensDoCarrinho.map(item => item.total).reduce((total, valor) => total + valor)
     }
+    
 }
 let carrinho = new Carrinho()
+
+// let numeros = [1, 3, 6, 20, 45, 33]
+// let chave;
+// let resultado = numeros.find((numero, index, array) => {
+//     chave = index;
+//     return numero >= 18;
+// })
+
+// console.log(numeros.splice(chave, 1));
+// console.log(numeros)
+
+function atualizarValorTotal(){
+    let h3Valor = document.getElementById("h3Valor")
+    h3Valor.textContent = carrinho.calculaValorTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
 
 // function colocarProdutoNoCarrinho(nomeProduto, valorDoProduto, quantidadeDoProduto) {
 //     if (quantidadeDoProduto !== "") {
@@ -31,33 +50,33 @@ let carrinho = new Carrinho()
 //     }
 // }
 
-function calcularValorTotalDoCarrinho() {
-    let soma = 0
+// function calcularValorTotalDoCarrinho() {
+//     let soma = 0
 
-    for (let i = 0; i < carrinho.length; i++) {
-        let produtoNoEspacoAtualNoCarrinho = carrinho[i]
-        // a gente só tá calculando o preço X quantidade daquele produto no carrinho
-        let valorTotalDoProdutoAtual = produtoNoEspacoAtualNoCarrinho.preco * produtoNoEspacoAtualNoCarrinho.quantidade
+//     for (let i = 0; i < carrinho.length; i++) {
+//         let produtoNoEspacoAtualNoCarrinho = carrinho[i]
+//         // a gente só tá calculando o preço X quantidade daquele produto no carrinho
+//         let valorTotalDoProdutoAtual = produtoNoEspacoAtualNoCarrinho.preco * produtoNoEspacoAtualNoCarrinho.quantidade
 
-        soma = soma + valorTotalDoProdutoAtual
-    }
+//         soma = soma + valorTotalDoProdutoAtual
+//     }
 
-    console.log("O valor total do carrinho é de R$ " + soma)
-    // let inputOndeFicaValorTotalDoCarrinho = document.getElementById("valorTotalDoCarrinho")
-    let h3Valor = document.getElementById("h3Valor")
+//     console.log("O valor total do carrinho é de R$ " + soma)
+//     // let inputOndeFicaValorTotalDoCarrinho = document.getElementById("valorTotalDoCarrinho")
+//     let h3Valor = document.getElementById("h3Valor")
 
-    // inputOndeFicaValorTotalDoCarrinho.value = soma
-    // toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }); serve pra formatar o número em dinheiro
-    h3Valor.innerHTML = soma.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
-}
+//     // inputOndeFicaValorTotalDoCarrinho.value = soma
+//     // toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }); serve pra formatar o número em dinheiro
+//     h3Valor.innerHTML = soma.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+// }
 
-function clicarComprarProduto(idDoCampoPreco, idDoCampoQuantidade, nomeDoProduto) {
-    let inputComPreco = document.getElementById(idDoCampoPreco)
-    let valorDoProduto = inputComPreco.value // onde fica que a maçã custa R$ 2,30
-    let quantidadeDoProduto = document.getElementById(idDoCampoQuantidade).value // aqui é onde o usuário diz quantas quer
+// function clicarComprarProduto(idDoCampoPreco, idDoCampoQuantidade, nomeDoProduto) {
+//     let inputComPreco = document.getElementById(idDoCampoPreco)
+//     let valorDoProduto = inputComPreco.value // onde fica que a maçã custa R$ 2,30
+//     let quantidadeDoProduto = document.getElementById(idDoCampoQuantidade).value // aqui é onde o usuário diz quantas quer
 
-    colocarProdutoNoCarrinho(nomeDoProduto, valorDoProduto, quantidadeDoProduto)
-}
+//     colocarProdutoNoCarrinho(nomeDoProduto, valorDoProduto, quantidadeDoProduto)
+// }
 
 //Criando Cadrs
 function criarCard(produtos){ 
@@ -71,11 +90,11 @@ function criarCard(produtos){
         </div>
         <div class="card">
             <div class="card-body">
-            <input class="precoProduto" type=number  value='${produto.preco}' disabled>
+            <input class="precoProduto" type="text"  value='${produto.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}' disabled>
             </div>
         </div>
         <div class="card-footer">
-            <input class="quantidadeProduto" type="number">
+            <input class="quantidadeProduto" type="number" min="1">
             <a href="#" data-id="${produto.id}" class="btn btn-primary">Comprar</a>
         </div>
         </div>
@@ -87,14 +106,24 @@ function criarCard(produtos){
 function criarLinhaNoCarrinho(produto){ 
     let listaProdutosCarrinho = document.querySelector("#listaProdutoNoCarrinho")
             listaProdutosCarrinho.innerHTML +=
-            `<tr>
-            <td><img src="${produto.img}"></td>
+            `<tr id="produtoCarrinho${produto.id}">
+            <td><img src="${produto.img}" class="imgTabela"></td>
             <td>${produto.nome}</td>
-            <td>${produto.quantidade}</td>
+            <td class="produtoQuantidade">${produto.quantidade}</td>
             <td>${produto.preco}</td>
-            <td>${produto.total}</td>
+            <td class="produtoValorTotal">${produto.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</td>
+            <td><button class="btn-Remover" "data-id="${produto.id}">Excluir</buttton></td>
             </tr> `
     
+}
+function atualizarLinhaNoCarrinho(produto){
+    let produtoNoCarrinhoEl = document.getElementById(`produtoCarrinho${produto.id}`)
+    let quantidadeEl = produtoNoCarrinhoEl.querySelector(".produtoQuantidade")
+    let valorTotalEl = produtoNoCarrinhoEl.querySelector(".produtoValorTotal")
+    //textContent (serve para mudar apenas o texto no html)
+    quantidadeEl.textContent = produto.quantidade
+    valorTotalEl.textContent = produto.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+
 }
 
 //Adicionando Eventos nos Botões dos Cards
@@ -108,12 +137,27 @@ function adicionarEventoNosBotoesDosCards(produtos){
             let produto = buscaProduto(id, produtos)    
             
             let quantidadedeProduto = parseInt(document.querySelector(`#produto${id} .quantidadeProduto`).value)
-            produto = carrinho.adicionarProdutoCarrinho({...produto, quantidade: quantidadedeProduto, total: quantidadedeProduto*produto.preco})
-            console.log(carrinho.listaProdutosdoCarrinho)
-            criarLinhaNoCarrinho(produto)
-        //     colocarProdutoNoCarrinho(nomedoProduto, valordoProduto, quantidadedeProduto)
-            console.log(carrinho.mostrarValorTotal)
-        
+            if(quantidadedeProduto > 0){
+                if(carrinho.itensDoCarrinho.length > 0){
+                    let produtoComMesmoId = carrinho.itensDoCarrinho.find(itemDoCarrinho => itemDoCarrinho.id === produto.id)
+                        
+                        if(produtoComMesmoId){
+                            produtoComMesmoId.quantidade += quantidadedeProduto
+                            produtoComMesmoId.total = produtoComMesmoId.preco * produtoComMesmoId.quantidade
+                            atualizarLinhaNoCarrinho(produtoComMesmoId)
+                        }else{
+                            produto = carrinho.adicionarProdutoCarrinho({...produto, quantidade: quantidadedeProduto, total: quantidadedeProduto*produto.preco})                    
+                            criarLinhaNoCarrinho(produto)
+                        }
+                }else{
+                    produto = carrinho.adicionarProdutoCarrinho({...produto, quantidade: quantidadedeProduto, total: quantidadedeProduto*produto.preco})                    
+                    criarLinhaNoCarrinho(produto)
+                }
+                atualizarValorTotal()
+            }else{
+                alert(`Por favor preencha uma quantidade valida do produto ${produto.nome}`)
+            }         
+                        
         })
     })
 }
